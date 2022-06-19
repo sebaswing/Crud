88b509140e925ae9e54c39d8cd821bd83294702b
sebaswing <beltry1@hotmail.com>
2022-06-19 16:10:46 -0300

    fix performance people-list

--- src/app/people-list/people-list.component.ts
+++ src/app/people-list/people-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { Vacuna } from '../Modelo/Vacuna';
 import { VacunasService } from '../services/vacunas.service';
 
@@ -14,103 +15,93 @@ enum nombreVacuna {
   templateUrl: './people-list.component.html',
   styleUrls: ['./people-list.component.css']
 })
-export class PeopleListComponent implements OnInit {
+export class PeopleListComponent implements OnInit, OnDestroy {
 
-  //filteredProducts:Persona[]=[];
-  //rivate _filterList='';
-  //get listFilter():string
-   // {
-   //   return this._filterList;
-   // }
-  //set listFilter(value:string)
-    //{
-    //  this._filterList=value;
-    //  this.filteredProducts=this.performFilter(value);
-   // }
-  public nombreVacunasEnum = nombreVacuna;
+  nombreVacunasEnum = nombreVacuna;
   vacunas: Vacuna[];
   vacunasPasadas: Vacuna[];
   covid:Vacuna[];
   gripe:Vacuna[];
   gripeAmarilla:Vacuna[];
-  mostrarBotonAmarilla:boolean;
-  mostrarCartelEsperando:boolean;
+  mostrarBotonAmarilla:boolean = false;
+  mostrarCartelEsperando:boolean = false;
+  suscriptions = new Subscription();
 
   constructor(
     private vacunaService:VacunasService,
     private route:Router
   ) {}
-  //performFilter(filterBy:string):Persona[]
-   //// {
-     // filterBy = filterBy.toLocaleLowerCase();
-     // return this.personas.filter((product:Persona)=>
-     // product.nombre.toLocaleLowerCase().includes(filterBy));
-    //}
 
   ngOnInit(): void {
-    this.mostrarBotonAmarilla=false;
-    this.mostrarCartelEsperando=false;
     const id = Number(localStorage.getItem('idPaciente')) || 0;
 
-    this.vacunaService.obtenerVacunas(id).subscribe(
-      vac => {
+    this.suscriptions.add(
+      this.vacunaService.obtenerVacunas(id).subscribe(vac => {
         this.vacunas = vac;
-        this.gripe= this.vacunas.filter(co=>co.id_vacuna==1 && !this.compararFechas(co));
-        this.covid= this.vacunas.filter(co=>co.id_vacuna==2 && !this.compararFechas(co));
-        this.gripeAmarilla= this.vacunas.filter(co=>co.id_vacuna==3);
-        this.mostrarBotonAmarilla=this.gripeAmarilla.length===0;
-        this.vacunasPasadas= this.vacunas.filter(co=> this.compararFechas(co) && this.fechaValida(co));
-        this.gripeAmarilla=this.gripeAmarilla.filter(co=>!this.compararFechas(co));
-      }
+        this.cargarListasVacunas();
+        this.mostrarBotonAmarilla = this.puedoSolicitarAmarilla();
+        this.mostrarCartelEsperando = this.puedoMostrarCartelEsperando();
+      })
     );
   }
-  edadValidaAmarilla(){
-    return Number(localStorage.getItem('edad'))>=18 && Number(localStorage.getItem('edad'))<60;
-  }
-  compararFechas(vac:Vacuna){
-    let FechaHoy=new Date(Date.now());
-    let fechaNcimiento=new Date(vac.fecha_aplicacion);
-    return fechaNcimiento.getTime()<FechaHoy.getTime();
+
+  cargarListasVacunas() {
+    this.gripe = this.vacunas.filter(vacuna => vacuna.id_vacuna === 1 && !this.compararFechas(vacuna));
+    this.covid = this.vacunas.filter(vacuna => vacuna.id_vacuna === 2 && !this.compararFechas(vacuna));
+    this.gripeAmarilla = this.vacunas.filter(vacuna => vacuna.id_vacuna === 3 && !this.compararFechas(vacuna) && this.fechaValida(vacuna));
+    this.vacunasPasadas = this.vacunas.filter(vacuna => this.compararFechas(vacuna) && this.fechaValida(vacuna));
   }
 
-  fechaValida(vac:Vacuna){
-    let fechaVacuna=new Date(vac.fecha_aplicacion);
-    if((fechaVacuna.getFullYear()+1)==1900)
-    {
-      this.mostrarCartelEsperando=true;
-    }
-    return fechaVacuna.getFullYear()+1!=1900;
+  puedoSolicitarAmarilla() {
+    const vacunasAmarilla = this.vacunas.filter(vacuna => vacuna.id_vacuna === 3);
+    const noSeVacuno = vacunasAmarilla.length === 0;
+    const noSolicitoTurno = vacunasAmarilla.filter(vacuna => !this.compararFechas(vacuna)).length === 0
+
+    return noSeVacuno && noSolicitoTurno && this.edadValidaAmarilla();
   }
 
-  mostrarAConfirmar(){
-    return this.mostrarCartelEsperando;
-  }
-  checkBotonAmarilla()
-  {
-    if(this.mostrarBotonAmarilla===true){
-    const id = Number(localStorage.getItem('idPaciente')) || 0;
-     this.vacunaService.obtenerVacunas(id).subscribe(
-      vac => {
-        this.vacunas = vac;
-        this.gripeAmarilla= this.vacunas.filter(co=>co.id_vacuna==3);
-        this.mostrarBotonAmarilla=this.gripeAmarilla.length===0;
-        this.mostrarCartelEsperando = this.vacunas.filter(co=>co.id_vacuna==3 && !this.compararFechas(co)&&this.fechaValida(co)).length!==0;
+  puedoMostrarCartelEsperando(): boolean {
+    const vacunasAmarilla = this.vacunas.filter(vacuna => vacuna.id_vacuna === 3);
 
-        }
-      )
-    }
-    return this.mostrarBotonAmarilla;
+    return !this.mostrarBotonAmarilla && vacunasAmarilla.length > 0 && !this.fechaValida(vacunasAmarilla[0]);
   }
 
-  solicitarTurnoAmarilla(){
-    let fiebre:any={};
+  edadValidaAmarilla(): boolean {
+    const edad = Number(localStorage.getItem('edad'))
+
+    return edad >= 18 && edad < 60;
+  }
+
+  compararFechas(vac:Vacuna): boolean {
+    const fechaHoy = new Date(Date.now());
+    const fechaTurno = new Date(vac.fecha_aplicacion);
+
+    return fechaTurno.getTime()<fechaHoy.getTime();
+  }
+
+  fechaValida(vac:Vacuna): boolean {
+    let fechaVacuna = new Date(vac.fecha_aplicacion).getUTCFullYear();
+
+    return fechaVacuna !== 1900;
+  }
+
+  solicitarTurnoAmarilla(): void {
+    const fiebre:any = {};
+
     fiebre.id_usuario=Number(localStorage.getItem('idPaciente'));
     fiebre.id_vacuna=3;
     fiebre.dosis=1;
     fiebre.fecha_aplicacion=new Date("1900-01-01");
     fiebre.observacion='';
-    this.vacunaService.createVacuna(fiebre).subscribe();
-    this.route.navigate(['refresh']);
+    this.suscriptions.add(
+      this.vacunaService.createVacuna(fiebre).subscribe({
+        next: ()=> this.route.navigate(['refresh']),
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.suscriptions.unsubscribe();
   }
 
 }
